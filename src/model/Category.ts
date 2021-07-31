2dac18152cee1cceeb7fafd3512982a8e0e20418
hakuunabatata <emachadomartins1999@gmail.com>
2021-07-31 15:23:03 -0300

    check and list categories

--- src/model/Category.ts
+++ src/model/Category.ts
@@ -1,12 +1,16 @@
 import { v4 as uuid } from 'uuid'
 
+interface ICreateCategoryDTO {
+  name: string
+  description: string
+}
 class Category {
   id?: string
   name: string
   description: string
   created_at?: Date
 
-  constructor({ name, description }) {
+  constructor({ name, description }: ICreateCategoryDTO) {
     if (!this.id) this.id = uuid()
 
     this.created_at = new Date()
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,9 +8,20 @@ const categoriesRepository = new CategoryRepository()
 categoryRoutes.post('/', (req, res) => {
   const { name, description } = req.body
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name)
+
+  if (categoryAlreadyExists)
+    return res.status(400).json({ error: 'Category Already Exists!' })
+
   categoriesRepository.create({ name, description })
 
   return res.status(201).send()
+})
+
+categoryRoutes.get('/', (req, res) => {
+  const categories = categoriesRepository.list()
+
+  return res.json(categories)
 })
 
 export { categoryRoutes }
