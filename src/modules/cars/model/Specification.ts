75d8225d352635acab9477770d312597b23828e4
hakuunabatata <emachadomartins1999@gmail.com>
2021-08-10 20:43:09 -0300

    create cars module

---
+++ src/modules/cars/model/Specification.ts
@@ -0,0 +1,20 @@
+import { v4 as uuid } from 'uuid'
+
+import { ICreateSpecificationDTO } from '../repositories/ISpecificationRepository'
+
+class Specification {
+  id?: string
+  name: string
+  description: string
+  created_at?: Date
+
+  constructor({ name, description }: ICreateSpecificationDTO) {
+    if (!this.id) this.id = uuid()
+
+    this.created_at = new Date()
+    this.name = name
+    this.description = description
+  }
+}
+
+export { Specification }
