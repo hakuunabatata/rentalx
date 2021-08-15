44b890dc7f91ef8051231728eaaa996e11aca67f
hakuunabatata <emachadomartins1999@gmail.com>
2021-08-15 12:55:19 -0300

    create import controller and usecase

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,14 +1,24 @@
 import { Router } from 'express'
+import multer from 'multer'
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
+import { importCategoryController } from '../modules/cars/useCases/importCategory'
 import { listCategoriesController } from '../modules/cars/useCases/listCategories'
 
 const categoryRoutes = Router()
+
+const upload = multer({
+  dest: './tmp',
+})
 
 categoryRoutes.post('/', (req, res) =>
   createCategoryController.handle(req, res)
 )
 
 categoryRoutes.get('/', (req, res) => listCategoriesController.handle(req, res))
+
+categoryRoutes.post('/import', upload.single('file'), (req, res) => {
+  return importCategoryController.handle(req, res)
+})
 
 export { categoryRoutes }
